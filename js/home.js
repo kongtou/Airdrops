36e2a488d762a70d7445090d844e2a50e382e471
doubleDragon <aiai373824745@gmail.com>
2018-05-27 00:14:43 +0800

    Second commit

--- js/home.js
+++ js/home.js
@@ -87,4 +87,30 @@ jQuery(document).ready(function($){
 
         }
     }
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        direction: 'horizontal',
+        slidesPerView: 4,
+        spaceBetween: 10,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+
+
+        // 如果需要分页器
+        // pagination: {
+        //     el: '.swiper-pagination',
+        // },
+
+        // 如果需要前进后退按钮
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+        // 如果需要滚动条
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    })
 });
